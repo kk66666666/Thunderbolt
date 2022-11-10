a298c88bc3f2b7ebed5ea1a8773092461dc6eca3
kk66666666 <115142452+kk66666666@users.noreply.github.com>
2022-11-10 15:43:45 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,23 @@
-
+function _5 () {
+    y = 0
+    for (let index = 0; index < 5; index++) {
+        led.toggle(x, y)
+        y += 1
+    }
+}
+let y = 0
+let x = 0
+x = 0
+y = 0
+let 左右 = 1
+basic.forever(function () {
+    _5()
+    basic.pause(200)
+    _5()
+    x += 左右
+    if (x == 4) {
+        左右 = -1
+    } else if (x == 0) {
+        左右 = 1
+    }
+})
