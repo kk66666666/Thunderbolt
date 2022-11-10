313fa5fab382e4e1e0339fa95f330bda3c822ee9
kk66666666 <115142452+kk66666666@users.noreply.github.com>
2022-11-10 16:46:30 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,8 +1,8 @@
 function _5 () {
     y = 0
-    for (let index = 0; index < 5; index++) {
-        led.toggle(x, y)
-        y += 1
+    for (let index = 0; index < 3; index++) {
+        led.plotBrightness(x, y, 255)
+        y += 2
     }
 }
 let y = 0
@@ -11,13 +11,15 @@ x = 0
 y = 0
 let 左右 = 1
 basic.forever(function () {
-    _5()
-    basic.pause(100)
-    _5()
-    x += 左右
-    if (x == 4) {
-        左右 = -1
-    } else if (x == 0) {
-        左右 = 1
+    for (let index = 0; index < 3; index++) {
+        _5()
+        basic.pause(100)
+        basic.clearScreen()
+        x += 左右
+        if (x == 4) {
+            左右 = -1
+        } else if (x == 0) {
+            左右 = 1
+        }
     }
 })
