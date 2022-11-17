22a50efe271580b5e841ea583e5cc4f7d93764cd
kk66666666 <115142452+kk66666666@users.noreply.github.com>
2022-11-17 16:16:58 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,25 +1,26 @@
-function _5 () {
-    y = 0
-    for (let index = 0; index < 3; index++) {
-        led.plotBrightness(x, y, 255)
-        y += 2
-    }
-}
+let 亮度 = 0
+let n = 0
 let y = 0
 let x = 0
-x = 0
-y = 0
 let 左右 = 1
 basic.forever(function () {
-    for (let index = 0; index < 3; index++) {
-        _5()
-        basic.pause(100)
-        basic.clearScreen()
-        x += 左右
-        if (x == 4) {
-            左右 = -1
-        } else if (x == 0) {
-            左右 = 1
-        }
+    basic.clearScreen()
+    if (x == 4) {
+        左右 = -1
+    } else if (x == 0) {
+        左右 = 1
     }
+    x += 左右
+    y = 0
+    for (let index = 0; index < 5; index++) {
+        for (let index = 0; index < 5; index++) {
+            led.plotBrightness(x - 左右 * n, y, 亮度)
+            亮度 += -50
+            n += 1
+        }
+        n = 0
+        y += 1
+        亮度 = 255
+    }
+    basic.pause(100)
 })
