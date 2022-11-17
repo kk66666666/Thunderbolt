1ac9e638cffe591627f51e5c0a5df719bdf4f7e1
kk66666666 <115142452+kk66666666@users.noreply.github.com>
2022-11-17 16:20:27 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -5,13 +5,13 @@ let x = 0
 let 左右 = 1
 basic.forever(function () {
     basic.clearScreen()
+    y = 0
     if (x == 4) {
         左右 = -1
     } else if (x == 0) {
         左右 = 1
     }
     x += 左右
-    y = 0
     for (let index = 0; index < 5; index++) {
         for (let index = 0; index < 5; index++) {
             led.plotBrightness(x - 左右 * n, y, 亮度)
